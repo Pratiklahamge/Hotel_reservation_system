b48eb6cd2ff7816b1a9e9b6c831626c681a5c883
pratikl <plahamge@gmail.com>
2021-11-07 16:31:42 +0530

    Added: Ability to find the cheapest Hotel for a given Date Range..

--- src/main/java/HotelReservation/com/HotelReservation/Hotel.java
+++ src/main/java/HotelReservation/com/HotelReservation/Hotel.java
@@ -1,12 +1,14 @@
 package HotelReservation.com.HotelReservation;
 
 public class Hotel {
-	  private String hotelName;
+	 private String hotelName;
+	    private String customerType;
 	    private int hotelRegularRate;
 	    private int hotelWeekendRate;
 
-	    public Hotel(String hotelName,int hotelRegularRate,int hotelWeekendRate){
+	    public Hotel(String hotelName,String customerType, int hotelRegularRate,int hotelWeekendRate){
 	        this.hotelName = hotelName;
+	        this.customerType = customerType;
 	        this.hotelRegularRate = hotelRegularRate;
 	        this.hotelWeekendRate = hotelWeekendRate;
 	    }
@@ -17,6 +19,14 @@ public class Hotel {
 
 	    public void setHotelName(String hotelName){
 	        this.hotelName = hotelName;
+	    }
+
+	    public String getCustomerType(){
+	        return customerType;
+	    }
+
+	    public void setCustomerType(String customerType){
+	        this.customerType = customerType;
 	    }
 
 	    public int getHotelRegularRate(){
@@ -35,3 +45,4 @@ public class Hotel {
 	        this.hotelWeekendRate = hotelWeekendRate;
 	    }
 	}
+
